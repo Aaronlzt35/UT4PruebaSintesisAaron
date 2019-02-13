7e67e9b235f92655909bc8ce387a7a82656f121a
Aaronlzt35 <47208496+Aaronlzt35@users.noreply.github.com>
2019-02-13 09:33:08 +0000

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author U_35007842
+ */
+public class Calculadora {
+    private int operador1;
+    private int oper2;
+    private int operador;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    /**
+     * @return the operador1
+     */
+    public int getOperador1() {
+        return operador1;
+    }
+
+    /**
+     * @param operador1 the operador1 to set
+     */
+    public void setOperador1(int operador1) {
+        this.operador1 = operador1;
+    }
+
+    /**
+     * @return the oper2
+     */
+    public int getOper2() {
+        return oper2;
+    }
+
+    /**
+     * @param oper2 the oper2 to set
+     */
+    public void setOper2(int oper2) {
+        this.oper2 = oper2;
+    }
+
+    /**
+     * @return the operador
+     */
+    public int getOperador() {
+        return operador;
+    }
+
+    /**
+     * @param operador the operador to set
+     */
+    public void setOperador(int operador) {
+        this.operador = operador;
+    }
+    
+}
